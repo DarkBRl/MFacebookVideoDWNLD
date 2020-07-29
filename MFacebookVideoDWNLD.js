e3877670e6a8765781f026ce1343b6b8fe464781
Lucas Branco <dakimakurabr@gmail.com>
2020-07-29 11:57:08 -0300

    update 1.1.1

--- MFacebookVideoDWNLD.js
+++ MFacebookVideoDWNLD.js
@@ -1,17 +1,15 @@
 // ==UserScript==
 // @name         MFacebookVideoDWNLD
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.1.1
 // @description  Remove the attribute in the html video that prevents you from downloading video on m.facebook
 // @author       Lucas Branco
 // @match        https://m.facebook.com/*
-// @grant        GM_log
-// @grant        GM_getValue
-// @grant        GM_setValue
+// @grant        none
 // @run-at       document-start
 // ==/UserScript==
 
-setInterval(function removeD() {
+setInterval(function() {
 	'use strict';
 	var video = document.getElementsByTagName("video");
 	video[0].removeAttribute("controlsList");
